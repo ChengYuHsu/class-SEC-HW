2970b2258cbc7c9412ea3476afc6dbd410702483
Soumya Ray <soumya.ray@gmail.com>
2015-03-13 18:25:56 +0800

    First draft of XOR_dfender assignment

---
+++ XOR_defender/xor_helper.rb
@@ -0,0 +1,9 @@
+# Author: Soumya Ray (soumya.ray@gmail.com) <-- change this to your teammates!
+
+# XorHelper provides an encode method to xor a file with a key
+module XorHelper
+  def self.encode(doc, key)
+    # Enter all your code into this method (only!)
+
+  end
+end
